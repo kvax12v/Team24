70de99ced4e74a8a62090fdfe785e058dda5ecc5
Jackson Tsoi <jtsoi1.@uci.edu>
2016-11-13 16:10:55 -0800

    Added NewUser Login Info

---
+++ app/src/main/java/com/medappjam/team24/meddo/Newuser_Login.java
@@ -0,0 +1,13 @@
+package com.medappjam.team24.meddo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Newuser_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newuser__login);
+    }
+}
