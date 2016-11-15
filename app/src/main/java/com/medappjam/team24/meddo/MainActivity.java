93d2f4b1ccfe07de4ccf8398a53e6de200cef311
kvax12v <kaylab@dhcp-v066-241.mobile.uci.edu>
2016-11-15 15:11:57 -0800

    Main Screen Button functionality

--- app/src/main/java/com/medappjam/team24/meddo/MainActivity.java
+++ app/src/main/java/com/medappjam/team24/meddo/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public void open_Newuser_Login(View view){
+            Intent intent1 = new Intent(this, Newuser_Login.class);
+            startActivity(intent1);
+    }
+
+    public void open_LoginActivity(View view){
+        Intent intent2 = new Intent(this, LoginActivity.class);
+        startActivity(intent2);
+
+    }
+
+
 }
