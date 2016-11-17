74eb02d2ccc7c6aaf871cee30c3efc0233fd1324
Jackson Tsoi <jtsoi1.@uci.edu>
2016-11-16 17:35:52 -0800

    Added Intent from Newuser_Login to MainActivity

--- app/src/main/java/com/medappjam/team24/meddo/Newuser_Login.java
+++ app/src/main/java/com/medappjam/team24/meddo/Newuser_Login.java
@@ -1,5 +1,6 @@
 package com.medappjam.team24.meddo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,5 +30,10 @@ public class Newuser_Login extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void open_MainActivity(View view) {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
     }
 }
